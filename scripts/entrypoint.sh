6b9efa8ee1850f8e4f13c173a4cb934dbb6bd3b3
Andrzej Pultyn <andrzej.pultyn.stud@pw.edu.pl>
2025-10-28 17:22:03 +0100

    Database info in starting script xD

--- scripts/entrypoint.sh
+++ scripts/entrypoint.sh
@@ -1,14 +1,9 @@
 #!/bin/bash
 
-# Database parameters
-DB_HOST=spring-jwt-db
-DB_USER=root
-DB_PASSWORD=my-secret-pwd
-
 echo "Waiting for database to be ready..."
 
 # Check database status
-while ! mysqladmin ping -h"$DB_HOST" -u"$DB_USER" -p"$DB_PASSWORD" --silent; do
+while ! mysqladmin ping -h"$DB_HOST" -u"$DB_USERNAME" -p"$DB_PASSWORD" --silent; do
     echo "Database is not ready yet. Retrying in 5 seconds..."
     sleep 5
 done
